78724a899498869927ffafcb1c882725ae9810f6
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-01 13:21:40 -0600

    Update preference.js

--- database/models/preference.js
+++ database/models/preference.js
@@ -1,1 +1,11 @@
-a
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Preference', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        user_id: { type: DataTypes.INTEGER, allowNull: false },
+        preferred_gender: { type: DataTypes.STRING },
+        age_min: { type: DataTypes.INTEGER, defaultValue: 18 },
+        age_max: { type: DataTypes.INTEGER, defaultValue: 99 }
+    });
+};a
