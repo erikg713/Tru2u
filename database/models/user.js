33f08704d2a6e016d7aa2ff8d8c117b3b14d628e
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-01 13:20:58 -0600

    Update user.js

--- database/models/user.js
+++ database/models/user.js
@@ -1,1 +1,10 @@
-users
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('User', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        email: { type: DataTypes.STRING, unique: true, allowNull: false },
+        password_hash: { type: DataTypes.STRING, allowNull: false },
+        created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+    });
+};users
