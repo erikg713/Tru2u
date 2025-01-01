744092af9d81828cfa09ea692c70534745e7e405
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-01 13:21:57 -0600

    Update match.js

--- database/models/match.js
+++ database/models/match.js
@@ -1,1 +1,11 @@
-t
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Match', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        user_id1: { type: DataTypes.INTEGER, allowNull: false },
+        user_id2: { type: DataTypes.INTEGER, allowNull: false },
+        matched_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+        is_active: { type: DataTypes.BOOLEAN, defaultValue: true }
+    });
+};t
