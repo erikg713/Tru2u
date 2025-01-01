a155e871022fccbc3e2927e59a7360bad246d550
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-01 13:22:12 -0600

    Update message.js

--- database/models/message.js
+++ database/models/message.js
@@ -1,1 +1,11 @@
-e
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Message', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        match_id: { type: DataTypes.INTEGER, allowNull: false },
+        sender_id: { type: DataTypes.INTEGER, allowNull: false },
+        message: { type: DataTypes.TEXT, allowNull: false },
+        sent_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+    });
+};e
