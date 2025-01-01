500fd97091c19b9cd447758a4f7dc34ca52e03f5
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-01 13:22:28 -0600

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,1 +1,8 @@
-2
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('tru2u', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+module.exports = sequelize;2
