19875a1ee81319b3fc0ca4174c2c4f65b414627c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 02:42:29 -0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Tru2u!</h1>
+      <p>Your trusted dating network on the Pi Network Web3 ecosystem.</p>
+    </div>
+  );
+};
+
+export default Home;
