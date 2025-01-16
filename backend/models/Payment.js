013eee505eef81ddca5260744067e32cf245e595
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 02:52:13 -0600

    Update Payment.js

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const PaymentSchema = new mongoose.Schema({
-  pi_payment_id: { type: String, required: true, unique: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  pi_payment_id: { type: String, required: true, unique: true },
   amount: { type: Number, required: true },
   status: { type: String, enum: ['pending', 'completed', 'cancelled'], default: 'pending' },
   createdAt: { type: Date, default: Date.now },
+  completedAt: { type: Date },
 });
 
 module.exports = mongoose.model('Payment', PaymentSchema);
