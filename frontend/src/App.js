5c80bbc5420b4b2e5ea888df385b04cfa6b8e511
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 02:12:12 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Chat from './pages/Chat';
+import Payment from './pages/Payment';
 
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
