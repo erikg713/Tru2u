6325d79529bcd3b256ef3ca42d537bd045f76dee
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 02:04:39 -0600

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,1 +1,9 @@
-1
+const express = require('express');
+const { createPayment, getUserPayments } = require('../controllers/paymentController');
+const { protect } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.post('/', protect, createPayment);
+router.get('/', protect, getUserPayments);
+
+module.exports = router;
