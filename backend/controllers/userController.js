149359793ff6e3dc72bec7b372e67454b6acebf3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 01:59:03 -0600

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,52 @@
-1
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+const registerUser = async (req, res) => {
+  const { username, uid, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ username });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ username, uid, password });
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      token: generateToken(user.id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user.id,
+        username: user.username,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+};
+
+const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+};
+
+module.exports = { registerUser, loginUser, getUserProfile };
