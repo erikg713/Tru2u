330405d3b324f14f82c360ff2e17a10c9c80e6cf
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 02:12:40 -0600

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,29 @@
-1
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    window.location.href = '/signin';
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/chat">Chat</Link></li>
+        <li><Link to="/payment">Payment</Link></li>
+        <li>
+          {localStorage.getItem('user') ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <Link to="/signin">Sign In</Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
