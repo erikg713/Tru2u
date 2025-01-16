20fffaeb39ccec916f978ccbcf5cd0a5dc619adc
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 01:29:42 -0600

    Delete backend directory

--- backend/tests/test_create_match.py
+++
@@ -1,6 +0,0 @@
-def test_create_match(session):
-    match = Match(user1_id=1, user2_id=2)
-    session.add(match)
-    session.commit()
-    assert match.id is not None
-    assert match.is_active is True
