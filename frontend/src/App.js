d448750ab09b489914bf10b8172f10cc2a14d235
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-27 14:08:05 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Chat from './pages/Chat';
-import Payment from './pages/Payment';
-
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/payment" element={<Payment />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Footer from "./components/Footer";
+import Login from "./pages/Login"; // Using "Login" for consistency
+import Signup from "./pages/Signup"; // Using "Signup" for consistency
+import Chat from "./pages/Chat";
+import Payment from "./pages/Payment";
 import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <AuthProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/payment" element={<Payment />} />
+        </Routes>
+        <Footer />
+      </Router>
     </AuthProvider>
   );
 };
