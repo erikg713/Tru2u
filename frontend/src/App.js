7d3626fcff6748132e11473f37932fae80e500cd
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-27 13:53:04 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,3 +25,29 @@ const App = () => {
 };
 
 export default App;
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Footer from "./components/Footer";
+import { AuthProvider } from "./context/AuthContext";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Footer />
+    </AuthProvider>
+  );
+};
+
+export default App;
