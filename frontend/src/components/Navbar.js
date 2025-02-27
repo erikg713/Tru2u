81eeffabd0f8c84070d80448806a2fa4fbb3865a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-27 13:53:39 -0600

    Update Navbar.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -27,3 +27,21 @@ const Navbar = () => {
 };
 
 export default Navbar;
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4 text-white flex justify-between">
+      <h1 className="text-lg font-bold">True-2-U</h1>
+      <div>
+        <Link className="mx-2" to="/">Home</Link>
+        <Link className="mx-2" to="/profile">Profile</Link>
+        <Link className="mx-2" to="/login">Login</Link>
+        <Link className="mx-2" to="/signup">Signup</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
